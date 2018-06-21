b993aea1b57a26e04606bfe0e10eefe98dee3ddd
nguyen.thi.quynh.mai <nguyen.thi.quynh.mai@b120570-mb.local>
2018-06-21 10:01:51 +0700

    Initial Commit

---
+++ ConfigEnviroments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConfigEnviroments
+//
+//  Created by nguyen.thi.quynh.mai on 6/21/18.
+//  Copyright © 2018 nguyen.thi.quynh.mai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
