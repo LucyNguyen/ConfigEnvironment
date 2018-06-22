4acee3d1536baeb05e8f64c8ebc76e2f0f82a85f
nguyen.thi.quynh.mai <nguyen.thi.quynh.mai@b120570-mb.local>
2018-06-22 16:16:47 +0700

    Config environment

---
+++ ConfigEnviroments/Environment.swift
@@ -0,0 +1,43 @@
+//
+//  Environment.swift
+//  ConfigEnviroments
+//
+//  Created by nguyen.thi.quynh.mai on 6/22/18.
+//  Copyright © 2018 nguyen.thi.quynh.mai. All rights reserved.
+//
+
+import Foundation
+
+public enum PlistKey {
+    case ServerURL
+    case ConnectionProtocol
+    
+    func value() -> String {
+        switch self {
+        case .ServerURL:
+            return "server_url"
+        case .ConnectionProtocol:
+            return "protocol"
+        }
+    }
+}
+public struct Environment {
+    
+    fileprivate var infoDict: [String: Any]  {
+        get {
+            if let dict = Bundle.main.infoDictionary {
+                return dict
+            }else {
+                fatalError("Plist file not found")
+            }
+        }
+    }
+    public func configuration(_ key: PlistKey) -> String {
+        switch key {
+        case .ServerURL:
+            return infoDict[PlistKey.ServerURL.value()] as! String
+        case .ConnectionProtocol:
+            return infoDict[PlistKey.ConnectionProtocol.value()] as! String
+        }
+    }
+}
